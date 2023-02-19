8d213abbb86b331a4e9e57f3291acbc26faa5d00
Jaswanth <107790919+Jaswanth77@users.noreply.github.com>
2023-02-19 23:11:20 +0530

    Add files via upload

---
+++ exp4/program2.js
@@ -0,0 +1,14 @@
+function duplicateArray()
+{
+    var array=[];
+    var elem=document.getElementById("arrayip").value;
+    for(let i=0;i<elem;i++)
+    {
+        var elem=prompt("enter no.:")
+        array.push(elem);
+    }
+    console.log(array);
+    const duplicates=array.filter((item,index) =>array.indexOf(item));
+    console.log("duplicates are:");
+    console.log(duplicates);
+}
